afb239b19ec96699fbe9481ba686fa139cd117f4
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2026-03-26 01:54:42 +0530

    feat: prettier setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Button } from "@base-ui/react/button";
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from '@base-ui/react/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+   useEffect(() => {
+      fetch('api/hello')
+         .then(res => res.json())
+         .then(data => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">{message}</h1>
-      <Button>Click Me</Button>
-    </div>
-  );
+   return (
+      <div className="flex flex-col items-center justify-center h-screen">
+         <h1 className="text-3xl font-bold">{message}</h1>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
 export default App;
